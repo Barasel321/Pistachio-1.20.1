77edb741ed9edbd34005126eaa2b762c335bf1ef
Barasel321 <barsoumfikry@hotmail.com>
2023-07-16 17:21:19 -0400

    Initial commit

---
+++ src/main/java/net/barasel321/pistachio/PistachioDataGenerator.java
@@ -0,0 +1,11 @@
+package net.barasel321.pistachio;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PistachioDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
